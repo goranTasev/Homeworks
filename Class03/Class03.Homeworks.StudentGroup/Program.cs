59928fbd61ea038cb5221f2b9281aea75714ac9b
Goran Tasev <goran.tasev18@gmail.com>
2020-05-03 21:24:08 +0200

    Third homework

---
+++ Class03/Class03.Homeworks.StudentGroup/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Class03.Homeworks.StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[] { "Goran", "Oli", "Darko", "Marko", "Sarko" };
+            string[] studentsG2 = new string[] { "Bojan", "Stojan", "Aleksandar", "Viktor", "Marija" };
+
+            Console.WriteLine("Enter student group: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 1 )
+            {
+                Console.WriteLine("The Students in G1 are: ");
+                for(int i = 0; i < studentsG1.Length; i++)
+                {
+                    Console.WriteLine(studentsG1[i]);
+                }
+            } 
+            else if (input == 2)
+            {
+                Console.WriteLine("The Students in G2 are: ");
+                for (int i = 0; i < studentsG2.Length; i++)
+                {
+                    Console.WriteLine(studentsG2[i]);
+                }
+            } else
+            {
+                Console.WriteLine("Wrong number!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class03/Class03.Homeworks.SumOfEven/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Class03.Homeworks.SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = new int[6];
+       
+            Console.Write("Enter integer no.1: ");
+            numbersArray[0] = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter integer no.2: ");
+            numbersArray[1] = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter integer no.3: ");
+            numbersArray[2] = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter integer no.4: ");
+            numbersArray[3] = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter integer no.5: ");
+            numbersArray[4] = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter integer no.6: ");
+            numbersArray[5] = int.Parse(Console.ReadLine());
+           
+
+            int sumOfEven = 0;
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                if (numbersArray[i] % 2 == 0)
+                {
+                    sumOfEven += numbersArray[i];
+                }
+            }
+
+            Console.WriteLine("The result is: " + sumOfEven);
+
+
+        }
+    }
+}
