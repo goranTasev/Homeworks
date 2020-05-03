d4ea3c0ddfe6467926a0effaa3f00dd36e470205
Goran Tasev <goran.tasev18@gmail.com>
2020-05-03 17:10:56 +0200

    My very first commit

---
+++ Class01/Class01.Homework/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Class01.Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trafic lights..");
+            Console.WriteLine();
+
+            System.Threading.Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Red light - STOP!");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine();
+            
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Yellow light - PREPARE!");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Green light - GO!");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(3000);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
